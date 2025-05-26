f090e9dc4d41c6dc0861b6426c57a1045407eb1f
EstherMutua <esthermutua16@gmail.com>
2025-05-26 12:01:03 +0300

    freebie tracker

---
+++ migrations/versions/f1b913cf8309_create_freebies_table.py
@@ -0,0 +1,24 @@
+"""create freebies table
+
+Revision ID: f1b913cf8309
+Revises: 
+Create Date: 2025-05-25 23:59:07.980468
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f1b913cf8309'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
